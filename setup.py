cd30b188fad5acedf53ad31a1a0eeb940bec2f93
unknown <shahirlatif1@gmail.com>
2022-06-29 15:16:03 -0500

    Fix audio dependency

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name = "beatgen",
     author = "SL",
     url = "https://github.com/sixilvr/beatgen",
     packages = ["beatgen"],
-    requires = ["audio @ git+https://github.com/sixilvr/audio", "numpy"]
+    requires = ["audio", "numpy"]
 )
